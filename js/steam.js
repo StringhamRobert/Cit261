a6cdb1ee7d83c70efc39973aceb18e6bd0623022
dragonballz789 <38597123+dragonballz789@users.noreply.github.com>
2019-02-05 15:46:23 -0700

    steam

--- js/steam.js
+++ js/steam.js
@@ -14,7 +14,16 @@ steamrequest.onload = function() {
     document.getElementById('pokename').innerHTML = pokemonheight;
     var pokemontype = steamdata.pokemon[24].type;
     document.getElementById('pokepic').innerHTML = pokemontype;
-    
+    var pokemonimage = steamdata.pokemon[24].img;
+    document.getElementById('pokeimage').src = pokemonimage;
+    var squirid = steamdata.pokemon[6].id;
+    document.getElementById('squid').innerHTML = squirid;
+    var squirheight = steamdata.pokemon[6].name;
+    document.getElementById('squname').innerHTML = squirheight;
+    var squirtype = steamdata.pokemon[6].type;
+    document.getElementById('squpic').innerHTML = squirtype;
+    var squirimage = steamdata.pokemon[6].img;
+    document.getElementById('squimage').src = squirimage;
     
     
     
