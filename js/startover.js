06e5e395208d12fcefe6b168f0932b2b43ab315b
dragonballz789 <38597123+dragonballz789@users.noreply.github.com>
2019-02-17 08:49:37 -0700

    d

--- js/startover.js
+++ js/startover.js
@@ -1,4 +1,4 @@
 function startover()
 {
-    document.getElementById('startover').window.location.assign("https://stringhamrobert.github.io/Cit261/practice/play.html")
+    window.location.assign("https://stringhamrobert.github.io/Cit261/practice/play.html")
 }
